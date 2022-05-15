7c92fb66a06ae2da13cecf1e8df9726ab9fc3046
snow <smashdojo3@gmail.com>
2022-05-14 23:32:47 -0400

    se agrego bodyparser para poder usar postman

--- src/app.js
+++ src/app.js
@@ -3,15 +3,22 @@ require('./database/db')
 const express = require ('express');
 const router = require ('./route/router.js');
 const cors = require('cors');
-// const bodyParser = require('body-parser');
+const bodyParser = require('body-parser');
 const app = express();
 const PORT = process.env.PORT || 5000
 app.use(cors())
 app.use(express.json())
 
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json());
+
 app.use('/',router)
 
-
+app.use(bodyParser.urlencoded({
+    extended: true
+  }));
 app.listen(PORT,()=>{
     console.log(`backend montado con exito en el puerto ${PORT}`)
 })
