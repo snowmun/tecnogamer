5e6ac537b3e348ed471cf98836a9ba403a638c0e
snow <smashdojo3@gmail.com>
2022-05-14 23:12:33 -0400

    se crea base de datos

---
+++ src/dataBase/db.js
@@ -0,0 +1,12 @@
+require('dotenv').config();
+const mongoose = require('mongoose')
+
+const url = `mongodb+srv://snow:${process.env.MongoClave}@tecnogamer.x5opd.mongodb.net/tecno?retryWrites=true&w=majority`
+
+mongoose.connect(url)
+
+const db = mongoose.connection
+db.on('open', ()=>{ console.log("¡Conectado a MongoDB!")} )
+db.on('error', ()=>{ console.log("¡Error al conectar a MongoDB!")} )
+
+module.exports=db  
