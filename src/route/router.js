ecbc63d976dbd10890ddfda5dcbd40c1070d8ed2
snow <smashdojo3@gmail.com>
2022-05-15 00:02:44 -0400

    se crean rutas para el crud usuario

--- src/route/router.js
+++ src/route/router.js
@@ -2,9 +2,13 @@ const express = require('express');
 const router = express.Router();
 const controller = require ('../controller/UserController');
 
-
+router.get('/api/v0/user', controller.getUser)
 router.get('/api/v0/alluser',controller.getAll)
 router.post('/api/v0/register',controller.register)
+router.post('/api/v0/login',controller.login)
+router.delete('/api/v0/deleteuser/:id',controller.deleteUser)
+router.put('/api/v0/deleteuser/:id',controller.UpdateUser)
+
 
 
 module.exports = router;
