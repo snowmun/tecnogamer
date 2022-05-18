5085ef977d57a6b1a4d54a08e1ebaa17c9691c18
Hesllar <hesllar.linzmayer@gmail.com>
2022-05-18 11:20:25 -0400

    Creación de router de marca y categoria

--- src/route/router.js
+++ src/route/router.js
@@ -1,14 +1,25 @@
 const express = require('express');
 const router = express.Router();
-const controller = require ('../controller/UserController');
+const controllerUser = require ('../controller/UserController');
+const controllerMarcas = require ('../controller/marcaController');
+const controllerCategoria = require ('../controller/categoriaController');
 
-router.get('/api/v0/user/:id', controller.getUser)
-router.get('/api/v0/alluser',controller.getAll)
-router.post('/api/v0/register',controller.register)
-router.post('/api/v0/login',controller.login)
-router.delete('/api/v0/deleteuser/:id',controller.deleteUser)
-router.put('/api/v0/updateuser/:id',controller.UpdateUser)
+//Rutas Usuario
+router.get('/api/v0/user/:id', controllerUser.getUser);
+router.get('/api/v0/alluser',controllerUser.getAll);
+router.post('/api/v0/register',controllerUser.register);
+router.post('/api/v0/login',controllerUser.login);
+router.delete('/api/v0/deleteuser/:id',controllerUser.deleteUser);
+router.put('/api/v0/updateuser/:id',controllerUser.UpdateUser);
 
+//Rutas Marca
+router.get('/api/v0/allmarcas',controllerMarcas.getAll);
+router.get('/api/v0/marca/:id', controllerMarcas.getMarca);
+router.post('/api/v0/marca/register',controllerMarcas.register);
 
+//Rutas Categoria
+router.get('/api/v0/allCategoria',controllerCategoria.getAll);
+router.get('/api/v0/categoria/:id', controllerCategoria.getCategoria);
+router.post('/api/v0/categoria/register',controllerCategoria.register);
 
 module.exports = router;
