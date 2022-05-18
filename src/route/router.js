f7cf65ad19e9f2251cc2c4509dfad2de924746a9
snow <smashdojo3@gmail.com>
2022-05-17 22:55:31 -0400

    cambios en direccionamiento de api

--- src/route/router.js
+++ src/route/router.js
@@ -7,7 +7,7 @@ router.get('/api/v0/alluser',controller.getAll)
 router.post('/api/v0/register',controller.register)
 router.post('/api/v0/login',controller.login)
 router.delete('/api/v0/deleteuser/:id',controller.deleteUser)
-router.put('/api/v0/deleteuser/:id',controller.UpdateUser)
+router.put('/api/v0/updateuser/:id',controller.UpdateUser)
 
 
 
