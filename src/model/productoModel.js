2a49fcca6cd91d4834435fe4358542466537a323
snow <smashdojo3@gmail.com>
2022-05-19 19:21:59 -0400

    se crea modelo del producto

--- src/model/productoModel.js
+++ src/model/productoModel.js
@@ -1,13 +1,23 @@
-const {model , Schema} = require("mongoose");
+const {model , Schema,mongoose} = require("mongoose");
 
 const productoSchema = new Schema ({
     nombreProducto:{type:String,require: true},
     stock:{type:String,require: true , unique:true},
     precio:{type:String,require: true},
     descripcion:{type:String,require: true},
-    categoria:{type:String,require: true},
-    marca:{type:String,require: true},
+    categoria: [{
+        type: Schema.Types.ObjectId,
+        ref: 'Categoria',
+        autopopulate: true
+    }],
+    marca: [{
+        type: Schema.Types.ObjectId,
+        ref: 'Marca',
+        autopopulate: true
+    }],
 
 });
 
+productoSchema.plugin(require('mongoose-autopopulate'))
 module.exports = model('Producto', productoSchema);
+
