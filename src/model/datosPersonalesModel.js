8b158b04fc9196e5afe13a143a0ff4cb8392c139
Hesllar <hesllar.linzmayer@gmail.com>
2022-06-03 23:04:52 -0400

    Modificación modelo

--- src/model/datosPersonalesModel.js
+++ src/model/datosPersonalesModel.js
@@ -2,11 +2,15 @@ const {model , Schema} = require("mongoose");
 
 const datosPersonalesSchema = new Schema ({
     nombre:{type:String,require: true },
-    apellido:{type:String,require: true },
-    edad:{type:Number,require: true },
+    apellido:{type:String,require: true},
     rut:{type:String,require: true },
-    fono:{type:String,require: true },
+    fono:{type:String },
     correo:{type:String,require: true },
+    direccionId: [{
+        type: Schema.Types.ObjectId,
+        ref: 'Direccione',
+        autopopulate: true
+    }],
 });
 
 datosPersonalesSchema.plugin(require('mongoose-autopopulate'));
