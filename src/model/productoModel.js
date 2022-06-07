87e4f2d8911825f9a8a914025b12a291a06b0bc7
Hesllar <hesllar.linzmayer@gmail.com>
2022-06-06 22:36:50 -0400

    Modificación modelo producto

--- src/model/productoModel.js
+++ src/model/productoModel.js
@@ -2,7 +2,7 @@ const {model , Schema} = require("mongoose");
 
 const productoSchema = new Schema ({
     nombreProducto:{type:String,require: true},
-    stock:{type:String,require: true , unique:true},
+    stock:{type:String,require: true},
     precio:{type:String,require: true},
     descripcion:{type:String,require: true},
     categoriaId: [{
