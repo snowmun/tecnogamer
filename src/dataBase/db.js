6657013ad69f879ec5e70908dbb74e312392d9ca
snow <smashdojo3@gmail.com>
2022-08-19 22:16:08 -0400

    se agrego un espacios

--- src/dataBase/db.js
+++ src/dataBase/db.js
@@ -5,6 +5,7 @@ const url = `mongodb+srv://snow:${process.env.MongoClave}@tecnogamer.x5opd.mongo
 
 mongoose.connect(url)
 
+
 const db = mongoose.connection
 db.on('open', ()=>{ console.log("¡Conectado a MongoDB!")} )
 db.on('error', ()=>{ console.log("¡Error al conectar a MongoDB!")} )
